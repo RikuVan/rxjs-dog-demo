8f124dc9d5391d768c3c7e5b6dfb4298acdc4467
Richard Van Camp <richard.vancamp@monad.fi>
2021-10-18 14:55:48 +0300

    Handle mode switching with rxjs

--- src/lib/Mode.svelte
+++ src/lib/Mode.svelte
@@ -1,46 +1,11 @@
 <script lang="ts">
-  import { onMount, afterUpdate } from 'svelte'
   import Button from './Button.svelte'
+  import { createModeState, Theme } from '../state/mode'
 
-  const SCHEME = '(prefers-color-scheme: dark)'
-  enum Theme {
-    Dark = 'dark',
-    Light = 'light',
-  }
-
-  export let key = '_theme_'
-  export let theme: Theme = Theme.Light
-
-  const validTheme = (t: Theme) => Object.values(Theme).includes(t as Theme)
-
-  const handleThemeChange = (e: MediaQueryListEvent) => {
-    theme = e.matches ? Theme.Dark : Theme.Light
-  }
-
-  onMount(() => {
-    const darkMode = window.matchMedia(SCHEME)
-    const persistedTheme = localStorage[key]
-    if (validTheme(persistedTheme)) {
-      theme = persistedTheme
-    } else {
-      theme = darkMode.matches ? Theme.Dark : Theme.Light
-    }
-
-    darkMode.addEventListener('change', handleThemeChange)
-    return () => {
-      darkMode.removeEventListener('change', handleThemeChange)
-    }
-  })
-
-  afterUpdate(() => {
-    if (validTheme(theme)) {
-      localStorage[key] = theme
-    }
-  })
-  $: switchTheme = theme === Theme.Dark ? Theme.Light : Theme.Dark
-  $: document.body.className = theme
+  const { modeState$, buttonClicks$ } = createModeState()
+  $: switchTheme = $modeState$ === Theme.Dark ? Theme.Light : Theme.Dark
 </script>
 
-<Button on:click={() => (theme = switchTheme)}>
+<Button on:click={() => buttonClicks$.next(switchTheme)}>
   Switch to {switchTheme} mode
 </Button>
